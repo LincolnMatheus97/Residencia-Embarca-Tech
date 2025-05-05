7e8cc4a5790ff89c02c7e25f2bc24baae29a64b5
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-05-04 21:55:26 -0300

    :truck: chore: Modifico o .gitignore e arquivo principal

--- conexao_i2c/conexao_i2c.c
+++ conexao_i2c/conexao_i2c.c
@@ -18,56 +18,53 @@ void inic_comun_i2c(i2c_inst_t *i2c, uint sda, uint scl, uint baudrate)
     gpio_pull_up(scl);
 }
 
-uint8_t id_disp_conectado(i2c_inst_t *i2c)
+uint8_t ender_dispo_conect(i2c_inst_t *i2c)
 {
-    uint8_t end_dispo = 0x00;
-    for (uint8_t end_provisorio = 0x08; end_provisorio <= 0x77; end_provisorio++)
+    uint8_t end_disp = 0x00;
+    for (uint8_t end_atual = 0x08; end_atual <= 0x77; end_atual++)
     {
-        uint8_t dados_visto;
-        int bytes_lidos = i2c_read_blocking(i2c, end_provisorio, &dados_visto, 1, false);
-        if (bytes_lidos >= 0)
+        uint8_t dados_vistos;
+        int confirmacao = i2c_read_blocking(i2c, end_atual, &dados_vistos, 1, false);
+        if (confirmacao >= 0)
         {
-            end_dispo = end_provisorio;
+            end_disp = end_atual;
             break;
         }
     }
-    return end_dispo;
+    return end_disp;
 }
 
-void inic_display(ssd1306_t *display, uint16_t larg_disp, uint16_t alt_disp, uint8_t end_dispo, i2c_inst_t *i2c)
+void inic_display(ssd1306_t *display, uint16_t larg_disp, uint16_t alt_disp, uint8_t end_disp, i2c_inst_t *i2c)
 {
-    ssd1306_init(display, larg_disp, alt_disp, end_dispo, i2c);
+    ssd1306_init(display, larg_disp, alt_disp, end_disp, i2c);
     ssd1306_clear(display);
 }
 
-void exibir_display(ssd1306_t *display, uint32_t larg_text, uint32_t alt_text, uint32_t tam_font, const char *texto)
+void mostr_display(ssd1306_t *display, uint32_t larg_texto, uint32_t alt_texto, uint32_t font_texto, const char* texto)
 {
     ssd1306_clear(display);
-    ssd1306_draw_string(display, larg_text, alt_text, tam_font, texto);
+    ssd1306_draw_string(display, larg_texto, alt_texto, font_texto, texto);
     ssd1306_show(display);
 }
 
 int main()
 {
     stdio_init_all();
-
     inic_comun_i2c(CANAL_I2C, PIN_SDA, PIN_SCL, 400 * 1000);
-    uint8_t end_display = id_disp_conectado(CANAL_I2C);
     
-    inic_display(&display, 128, 64, end_display, CANAL_I2C);
+    uint8_t end_disply = ender_dispo_conect(CANAL_I2C);
+    inic_display(&display, 128, 64, end_disply, CANAL_I2C);
 
     int contador = 0;
 
     while (1)
     {
         char texto[20];
-        snprintf(texto, sizeof(texto), "COUNT %d", contador);
+        snprintf(texto, sizeof(texto), "Count %d", contador);
 
-        exibir_display(&display, 10, 32, 2, texto);
+        mostr_display(&display, 10, 32, 2, texto);
         (contador >= 20) ? contador = 0 : contador ++;
-
         sleep_ms(1000);
     }
-    
     return 0;
 }
