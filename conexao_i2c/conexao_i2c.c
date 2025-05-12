2bb8c2c98d0fea1be423a40e2c75b84dd561e786
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-05-11 23:04:42 -0300

    :test_tube: test: Criando novo teste

--- conexao_i2c/conexao_i2c.c
+++ conexao_i2c/conexao_i2c.c
@@ -3,68 +3,84 @@
 #include "hardware/i2c.h"
 #include "ssd1306/ssd1306.h"
 
-#define CANAL_I2C i2c1
-#define PIN_SDA 14
-#define PIN_SCL 15
+#define I2C_PORT i2c1
+#define SDA_PIN 14
+#define SCL_PIN 15
 
 ssd1306_t display;
 
-void inic_comun_i2c(i2c_inst_t *i2c, uint sda, uint scl, uint baudrate)
+void inic_bar_i2c(i2c_inst_t *i2c, uint sda, uint scl, uint baundrate)
 {
-    i2c_init(i2c, baudrate);
+    i2c_init(i2c, baundrate);
     gpio_set_function(sda, GPIO_FUNC_I2C);
     gpio_set_function(scl, GPIO_FUNC_I2C);
     gpio_pull_up(sda);
     gpio_pull_up(scl);
 }
 
-uint8_t ender_dispo_conect(i2c_inst_t *i2c)
+uint8_t end_disp_i2c(i2c_inst_t *i2c)
 {
-    uint8_t end_disp = 0x00;
+    uint8_t endereco = 0x00;
+
     for (uint8_t end_atual = 0x08; end_atual <= 0x77; end_atual++)
     {
         uint8_t dados_vistos;
         int confirmacao = i2c_read_blocking(i2c, end_atual, &dados_vistos, 1, false);
+
         if (confirmacao >= 0)
         {
-            end_disp = end_atual;
+            endereco = end_atual;
             break;
         }
         sleep_ms(50);
     }
-    return end_disp;
+    return endereco;
 }
 
 void inic_display(ssd1306_t *display, uint16_t larg_disp, uint16_t alt_disp, uint8_t end_disp, i2c_inst_t *i2c)
 {
     ssd1306_init(display, larg_disp, alt_disp, end_disp, i2c);
-    ssd1306_clear(display);
 }
 
-void mostr_display(ssd1306_t *display, uint32_t larg_texto, uint32_t alt_texto, uint32_t font_texto, const char* texto)
+void escrev_display(ssd1306_t *display, uint32_t larg_texto, uint32_t alt_texto, uint32_t tam_texto, const char* texto)
+{
+    ssd1306_draw_string(display, larg_texto, alt_texto, tam_texto, texto);
+}
+
+void exib_display(ssd1306_t *display)
+{
+    ssd1306_show(display);
+}
+
+void limp_display(ssd1306_t *display)
 {
     ssd1306_clear(display);
-    ssd1306_draw_string(display, larg_texto, alt_texto, font_texto, texto);
-    ssd1306_show(display);
 }
 
 int main()
 {
     stdio_init_all();
-    inic_comun_i2c(CANAL_I2C, PIN_SDA, PIN_SCL, 400 * 1000);
-    
-    uint8_t end_disply = ender_dispo_conect(CANAL_I2C);
-    inic_display(&display, 128, 64, end_disply, CANAL_I2C);
+    inic_bar_i2c(I2C_PORT, SDA_PIN, SCL_PIN, 400 * 1000);
 
+    uint8_t ender_disp = end_disp_i2c(I2C_PORT);
+    inic_display(&display, 128, 64, ender_disp, I2C_PORT);
+    
     int contador = 0;
 
     while (1)
     {
-        char texto[20];
-        snprintf(texto, sizeof(texto), "Count %d", contador);
+        limp_display(&display);
 
-        mostr_display(&display, 10, 32, 2, texto);
-        (contador >= 20) ? contador = 0 : contador ++;
+        char texto_1[20];
+        snprintf(texto_1, sizeof(texto_1), "Protc. Comunic. I2C", texto_1);
+        escrev_display(&display, 0, 0, 1, texto_1);
+        
+        char texto_2[20];
+        snprintf(texto_2, sizeof(texto_2), "CONT: %d", contador);
+        escrev_display(&display, 10, 32, 2, texto_2);
+
+        exib_display(&display);
+        (contador >= 20) ? contador = 0 : contador++;
         sleep_ms(1000);
     }
     return 0;
